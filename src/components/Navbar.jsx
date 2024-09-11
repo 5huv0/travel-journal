e45b4157e659900a682f871da8e0dd2fb27fa8c0
5huv0 <md88748874@gmail.com>
2024-09-12 02:48:09 +0600

    navbar almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,38 @@ import React from "react";
 
 export default function Navbar(){
     return(
-        <nav></nav>
+        <nav className="nav-bar">
+            <img src="./public/images/logo-2.jpeg" alt="nav-logo" className="nav-logo"/>
+            <ul className="nav justify-content-end">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">About me</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Catagory
+                    </a>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <a className="dropdown-item" href="#">Solo Travel</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mount Travel</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Ocean Travel</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Foreign Travel</a>
+                        </li>
+                    </ul>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Blog</a>
+                </li>
+            </ul>
+        </nav>
     )
 }
