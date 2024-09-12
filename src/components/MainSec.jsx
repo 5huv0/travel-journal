47379a5aa2b571d265650b2df789e98acc3a2842
5huv0 <md88748874@gmail.com>
2024-09-12 15:25:58 +0600

    starting main section

--- src/components/MainSec.jsx
+++ src/components/MainSec.jsx
@@ -3,12 +3,30 @@ import React from "react";
 export default function Navbar(){
     return(
         <div>
-            <div className="coxs"></div>
-            <div className="saint"></div>
-            <div className="lalbagh"></div>
-            <div className="sundarban"></div>
-            <div className="sajek"></div>
-            <div className="ratargul"></div>
+            <div className="coxs">
+                <img src="" alt="" />
+            </div>
+
+            <div className="saint">
+                <img src="" alt="" />
+            </div>
+
+            <div className="lalbagh">
+                <img src="" alt="" />
+            </div>
+
+            <div className="sundarban">
+                <img src="" alt="" />
+            </div>
+
+            <div className="sajek">
+                <img src="" alt="" />
+            </div>
+
+            <div className="ratargul">
+                <img src="" alt="" />
+            </div>
+            
         </div>
     )
 }
