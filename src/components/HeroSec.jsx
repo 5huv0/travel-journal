7e7f050d779da87205eedf7cbe13605a0bfdaafe
5huv0 <md88748874@gmail.com>
2024-09-12 15:24:08 +0600

    herosec  done

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function HeroSec(){
     return(
-        <section className="hero-sec">
+        <div className="hero-sec">
             <h1>Where will you go next?</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, commodi perferendis maiores labore optio eius, voluptatum soluta architecto recusandae porro quae. Molestiae aspernatur unde necessitatibus quis esse voluptates veritatis repellat vitae excepturi? Provident tenetur vel, qui ut culpa, eligendi voluptatum ex, iure nostrum distinctio impedit inventore. Inventore, rerum accusamus nobis consequatur consequuntur corporis aperiam dolore aspernatur itaque ipsum numquam beatae obcaecati quia sed a eos corrupti in laboriosam fugiat. Nam iusto quod harum in quo ipsam laborum amet sed earum libero, asperiores consectetur possimus voluptate tempora nihil voluptatum facilis est praesentium architecto dignissimos obcaecati? Nisi repellat dolor labore dolorum eaque.</p>
             <h3>Lets Go.....</h3>
-        </section>
+        </div>
     )
 }
